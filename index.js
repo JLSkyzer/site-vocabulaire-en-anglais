e7c4c90d6b9185681e86bf993ddfc31ac81505e8
JLSkyzer <killiandasneves4@gmail.com>
2023-04-23 17:10:29 +0200

    update

--- index.js
+++ index.js
@@ -15,13 +15,76 @@ containArr.forEach(item => {
   item.contain.parentNode.appendChild(item.contain);
 });
 
+// ===========================================================
+
 // Récupération des éléments HTML nécessaires
-const container = document.querySelector('.container');
+const containerFR = document.querySelector('.containerFR');
+const containerEN = document.querySelector('.containerEN');
 const popUp = document.createElement('div');
 popUp.classList.add('popup');
 
+// Récupération des éléments HTML nécessaires
+const frBtn = document.getElementById('fr-btn');
+const enBtn = document.getElementById('en-btn');
+const searchbarFR = document.getElementById('searchbarFR');
+const searchbarEN = document.getElementById('searchbarEN');
+const toggle = "fr";
+
+// Ajout d'un événement de clic sur le bouton français
+frBtn.addEventListener('click', () => {
+  frBtn.classList.add('active');
+  enBtn.classList.remove('active');
+  containerFR.style.display = "flex"
+  containerEN.style.display = "none"
+  searchbarFR.style.display = "flex"
+  searchbarEN.style.display = "none"
+});
+
+// Ajout d'un événement de clic sur le bouton anglais
+enBtn.addEventListener('click', () => {
+    frBtn.classList.remove('active');
+    enBtn.classList.add('active');
+    containerFR.style.display = "none"
+    containerEN.style.display = "flex"
+    searchbarFR.style.display = "none"
+    searchbarEN.style.display = "flex"
+});
+
+function searchFR() {
+  let input = document.getElementById('searchbarFR').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('contain');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";                 
+      }
+  }
+}
+function searchEN() {
+  let input = document.getElementById('searchbarEN').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('contain');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="list-item";                 
+      }
+  }
+}
+
+
+// ================================================================
+
 // Ajout d'un événement de clic sur chaque div
-container.querySelectorAll('.contain').forEach((div) => {
+// FR
+containerFR.querySelectorAll('.contain').forEach((div) => {
   div.addEventListener('click', (event) => {
     // Récupération de la div interne "content"
     const content = event.currentTarget.querySelector('.content').innerHTML;
@@ -30,7 +93,7 @@ container.querySelectorAll('.contain').forEach((div) => {
     popUp.innerHTML = content;
     document.body.appendChild(popUp);
     popUp.classList.add('visible');
-    container.classList.add('disable-contain');
+    containerFR.classList.add('disable-contain');
     popUp.style.animationName = "popup-open";
     popUp.style.animationDuration = "0.2s"
     popUp.style.animationDirection = "forward"
@@ -46,7 +109,38 @@ container.querySelectorAll('.contain').forEach((div) => {
       popUp.style.animationTimingFunction = "ease-in-out";
       setTimeout(() => {
         popUp.classList.remove('visible');
-        container.classList.remove('disable-contain');
+        containerFR.classList.remove('disable-contain');
+      }, 149);
+    });
+  });
+});
+// EN
+containerEN.querySelectorAll('.contain').forEach((div) => {
+  div.addEventListener('click', (event) => {
+    // Récupération de la div interne "content"
+    const content = event.currentTarget.querySelector('.content').innerHTML;
+    
+    // Affichage de la popup avec le contenu de la div cliquée
+    popUp.innerHTML = content;
+    document.body.appendChild(popUp);
+    popUp.classList.add('visible');
+    containerEN.classList.add('disable-contain');
+    popUp.style.animationName = "popup-open";
+    popUp.style.animationDuration = "0.2s"
+    popUp.style.animationDirection = "forward"
+    popUp.style.animationTimingFunction = "ease-in-out";
+
+    // Récupération du bouton de fermeture
+    const closeBtn = popUp.querySelector('.closepopup');
+    // Ajout d'un événement de clic sur le bouton de fermeture
+    closeBtn.addEventListener('click', (event) => {
+      popUp.style.animationName = "popup-close";
+      popUp.style.animationDuration = "0.15s"
+      popUp.style.animationDirection = "backward"
+      popUp.style.animationTimingFunction = "ease-in-out";
+      setTimeout(() => {
+        popUp.classList.remove('visible');
+        containerEN.classList.remove('disable-contain');
       }, 149);
     });
   });
